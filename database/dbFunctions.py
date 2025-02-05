2b939f0902860259670d441c3bcb8b88c696c53b
Sophiyas061 <sophiya.singh@uconn.edu>
2025-02-04 19:19:37 -0500

    Updated dbFunctions.py with deleteRow and queryTable functions

--- database/dbFunctions.py
+++ database/dbFunctions.py
@@ -11,4 +11,20 @@ def addToTable(dbName, tableName, newValue):
     conn.commit()
     conn.close()
 
-    
+'Deletes row from given table based on condition'
+def deleteRow(dbName, tableName, condition):
+    conn = sqlite3.connect(dbName)
+    cursor = conn.cursor()
+    cursor.execute(f'DELETE FROM {tableName} WHERE {condition}')
+    conn.commit()
+    conn.close()
+
+'Queries table and returns matching rows'
+def queryTable(dbName, tableName, condition='1=1'):
+    '1=1 is the default which checks all rows'
+    conn = sqlite3.connect(dbName)
+    cursor = conn.cursor()
+    cursor.execute(f'SELECT * FROM {tableName} WHERE {condition}')
+    results = cursor.fetchall()
+    conn.close()
+    return results
