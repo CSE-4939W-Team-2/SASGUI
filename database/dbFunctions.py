eb814498c94ccb9964caba75531215f97bc8b01c
Benjamin Zheng <157739765+BenZhengOP@users.noreply.github.com>
2025-02-11 16:35:13 -0500

    Added functions to add to 'scans' and 'users'. Changed add and delete to prevent SQL injection attack

--- database/dbFunctions.py
+++ database/dbFunctions.py
@@ -1,30 +1,45 @@
 import sqlite3 
+import json
 
-dbName = 'SDPDatabase.sqlite'
-userTable = 'users'
-scansTable = 'scans'
+db_name = 'SDPDatabase.sqlite'
+user_table = 'users'
+scans_table = 'scans'
 
-def addToTable(dbName, tableName, newValue):
-    conn = sqlite3.connect(dbName)
+'Adds row to given table'
+def add_to_table(db_name, table_name, new_values):
+    conn = sqlite3.connect(db_name)
     cursor = conn.cursor()
-    cursor.execute(f'insert into {tableName} Values ({newValue})')
+    placeholders = ", ".join(["?"] * len(new_values))
+    query = f"INSERT INTO {table_name} VALUES ({placeholders})"
+    cursor.execute(query, new_values)
     conn.commit()
     conn.close()
 
 'Deletes row from given table based on condition'
-def deleteRow(dbName, tableName, condition):
-    conn = sqlite3.connect(dbName)
+def delete_row(db_name, table_name, column_name, value):
+    conn = sqlite3.connect(db_name)
     cursor = conn.cursor()
-    cursor.execute(f'DELETE FROM {tableName} WHERE {condition}')
+    query = f"DELETE FROM {table_name} WHERE {column_name} = ?"
+    cursor.execute(query, (value,))
     conn.commit()
     conn.close()
 
 'Queries table and returns matching rows'
-def queryTable(dbName, tableName, condition='1=1'):
+def query_table(db_name, table_name, condition='1=1'):
     '1=1 is the default which checks all rows'
-    conn = sqlite3.connect(dbName)
+    conn = sqlite3.connect(db_name)
     cursor = conn.cursor()
-    cursor.execute(f'SELECT * FROM {tableName} WHERE {condition}')
+    cursor.execute(f'SELECT * FROM {table_name} WHERE {condition}')
     results = cursor.fetchall()
     conn.close()
     return results
+
+'Adds a user to the users table'
+def add_to_users(user_id):
+    add_to_table(db_name, user_table, (user_id))
+    
+'Adds a scan along with parameters to the scans table'
+def add_to_scans(user_id, file_path, curve_type, parameter_dict):
+    parameters_json = json.dumps(parameter_dict)
+    new_values = (user_id, file_path, curve_type, parameters_json)
+    add_to_table(db_name, user_table, new_values)
